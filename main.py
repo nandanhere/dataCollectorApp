afd5b457d005b4084f324d4b69bea5131291b9b3
Nandan <nanspyro@gmail.com>
2021-10-29 22:14:22 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,201 @@
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+from kivy.uix.screenmanager import Screen
+import platform
+from kivymd.icon_definitions import md_icons
+from kivymd.app import MDApp
+from kivymd.uix.list import OneLineIconListItem
+from kivymd.uix.label import MDLabel
+from kivy.clock import Clock
+import time,datetime
+from plyer import accelerometer,barometer,brightness,battery,compass,gps,gravity,gyroscope,humidity,light,proximity,spatialorientation,temperature
+from oscpy.client import OSCClient
+from oscpy.server import OSCThreadServer
+from kivy.utils import platform
+from kivy.lang import Builder
+# comment this if you are running on computer
+from jnius import autoclass
+sensorList = ["date","time","timestamp","floor","location","accelerometer" , "barometer" , "battery" , "brightness" , "compass" , "gps" ,
+     "gravity" , "gyroscope" , "humidity" , "light" , "proximity" , "spatialorientation","temperature" ]
+
+Builder.load_string(
+    '''
+#:import images_path kivymd.images_path
+
+
+<CustomOneLineIconListItem>
+
+    ThreeLineListItem:
+        text: "Three-line"
+
+
+<HomeScreen>
+
+    MDBoxLayout:
+        orientation: 'vertical'
+        spacing: dp(10)
+        padding: dp(20)
+
+        MDBoxLayout:
+            adaptive_height: True
+            MDTextField:
+                id: floor
+                hint_text: 'Enter the Floor'
+                on_text_validate: root.ids.location.focus = True
+        MDBoxLayout:
+            adaptive_height: True
+            MDTextField:
+                id: location
+                hint_text: 'Enter the location'
+                on_text_validate: root.submit()
+        MDBoxLayout:
+            adaptive_height: True
+            MDFlatButton:
+                id: submitbutton
+                text: "Enter Room and floor number"
+                increment_width: "300dp"
+                on_press: root.submit()
+                background_color: (0.5, 0.7, 0.5, 0.9)   
+
+        RecycleView:
+            id: rv
+            key_viewclass: 'viewclass'
+            key_size: 'height'
+
+            RecycleBoxLayout:
+                padding: dp(10)
+                default_size: None, dp(100)
+                default_size_hint: 1, None
+                size_hint_y: None
+                height: self.minimum_height
+                orientation: 'vertical'
+'''
+)
+
+ 
+
+class HomeScreen(Screen):
+    def __init__(self,client, **kw):
+        super().__init__(**kw)
+        self.sensorLabelList = {}
+        self.gpstext = ""
+        self.client = client
+        try: 
+            compass.enable() #type:ignore
+            accelerometer.enable() #type:ignore
+            barometer.enable() #type:ignore
+            gyroscope.enable() #type:ignore
+            gravity.enable() #type:ignore
+            humidity.enable() #type:ignore
+            light.enable() #type:ignore
+            proximity.enable() #type:ignore
+            temperature.enable() #type:ignore
+            spatialorientation.enable_listener() #type:ignore
+            gps.configure(on_location=self.gps_callback) #type:ignore
+            gps.start() #type:ignore
+        except Exception as ex: print("Error :", ex)
+        Clock.schedule_interval(self.clockFn, 3)
+
+    def gps_callback(self,**kwargs):
+            self.gpstext =  'lat: {lat}, lon: {lon},speed:{speed},bearing:{bearing},altitude:{altitude}'.format(**kwargs)
+    def submit(self):
+        floor,location = self.ids.floor.text,self.ids.location.text
+        print("the inputs are " , self.ids.floor.text, self.ids.location.text)
+        self.ids.submitbutton.text = "Floor : " + self.ids.floor.text + " Location: " + self.ids.location.text
+        ss = "{};{}".format(floor,location)
+        message = bytes(ss,'utf-8')
+        print(message)
+        self.client.send_message(b'/ping', [message,])
+
+    def clockFn(self,dt):
+        try: self.ids.rv.data[self.sensorLabelList["accelerometer"]]['secondary_text'] = str(accelerometer.acceleration)
+        except Exception as ex: print("accelerometer Error :", ex)
+        try: self.ids.rv.data[self.sensorLabelList['barometer']]['secondary_text'] = str(barometer.pressure)
+        except Exception as ex: print("barometer Error :", ex)
+        try: self.ids.rv.data[self.sensorLabelList['brightness']]['secondary_text'] = str(brightness.current_level()) #type:ignore WRITE_SETTINGS permisison needed 
+        except Exception as ex: print("brightness Error :", ex)
+        try: self.ids.rv.data[self.sensorLabelList['compass']]['secondary_text'] = str(compass.field_uncalib) #type:ignore you get the uncalibrated field along with iron bias estimation
+        except Exception as ex: print("compass Error :", ex)
+        #   On Android `INTERNET`, `ACCESS_FINE_LOCATION`, `ACCESS_COARSE_LOCATION` permissions are needed for gps
+
+        try: self.ids.rv.data[self.sensorLabelList['gps']]['secondary_text'] = self.gpstext
+        except Exception as ex: print("gps Error :", ex)
+        try: self.ids.rv.data[self.sensorLabelList['gravity']]['secondary_text'] = str(gravity.gravity) #type:ignore
+        except Exception as ex: print("gravity Error :", ex)
+        try: self.ids.rv.data[self.sensorLabelList['gyroscope']]['secondary_text'] = str(gyroscope.rotation)
+        except Exception as ex: print("gyroscope Error :", ex)
+        try:self.sensorLabelList['humidity'].text = "humidity : {}".format(humidity.tell) #type:ignore
+        except Exception as ex: print("humidity Error :", ex)
+        try: self.ids.rv.data[self.sensorLabelList['light']]['secondary_text'] = str(light.illumination) #type:ignore
+        except Exception as ex: print("light Error :", ex)
+        try: self.ids.rv.data[self.sensorLabelList['proximity']]['secondary_text'] = str(proximity.proximity) #type:ignore
+        except Exception as ex: print("proximity Error :", ex)
+        try:self.ids.rv.data[self.sensorLabelList['spatialorientation']]['secondary_text'] = str(spatialorientation.orientation) #type:ignore
+        except Exception as ex: print("spatialorientation Error :", ex)
+        try: self.ids.rv.data[self.sensorLabelList['temperature']]['secondary_text'] = str(temperature.temperature) #type:ignore
+        except Exception as ex: print("temperature Error :", ex)
+        try : self.ids.rv.data[self.sensorLabelList['battery']]['secondary_text'] = str(battery.status) #BATTERY_STATS permission needed
+        except Exception as ex: print("battery Error :", ex)
+        
+
+        t = time.time()
+        dt = datetime.datetime.fromtimestamp(t)
+        self.ids.rv.data[self.sensorLabelList['date']]['secondary_text'] = dt.strftime("%d/%m/%Y")
+        self.ids.rv.data[self.sensorLabelList['time']]['secondary_text'] = dt.strftime("%H:%M:%S")
+        self.ids.rv.data[self.sensorLabelList['timestamp']]['secondary_text'] = str(time.time())
+        self.ids.rv.refresh_from_data()
+
+    def buildlist(self):
+        self.sensorLabelList = {}
+        i = 0
+        for id in sensorList:
+            self.ids.rv.data.append(
+                {
+                    "viewclass": "ThreeLineListItem",
+                    "id": id,
+                    "text": id,
+                    "secondary_text": self.sensorLabelList.get(id,"None"), #type:ignore
+                }
+            )
+            self.sensorLabelList[id] = i
+            i += 1
+
+
+class MainApp(MDApp):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.server = server = OSCThreadServer()
+        server.listen(
+                address=b'localhost',
+                port=3002,
+                default=True,
+            )
+        server.bind(b'/message', self.callback)
+        self.client = OSCClient(b'localhost', 3000)
+        if platform == 'android':
+            SERVICE_NAME = u'{packagename}.Service{servicename}'.format(packagename=u'org.kivy.datacollector',servicename=u'Datacollectorservice')
+            service = autoclass(SERVICE_NAME)
+            self.mActivity = autoclass(u'org.kivy.android.PythonActivity').mActivity
+            argument = ''
+            service.start(self.mActivity, argument)
+            self.service = service
+        self.screen = HomeScreen(self.client)
+
+    def build(self):
+        return self.screen
+    def callback(self):
+        # Not needed in this program. added just in case its needed in the future
+        pass
+
+    def on_start(self):
+        self.screen.buildlist()
+
+
+
+if __name__=='__main__':
+    if platform == "android":
+        from android.permissions import request_permissions,Permission,check_permission            #type: ignore
+        # since service needs these perms we need to ask through ui 
+        request_permissions([Permission.WRITE_EXTERNAL_STORAGE,Permission.READ_EXTERNAL_STORAGE,Permission.WRITE_SETTINGS,Permission.ACCESS_FINE_LOCATION,Permission.ACCESS_COARSE_LOCATION])
+        
+    MainApp().run()
---
+++ service.py
@@ -0,0 +1,132 @@
+import csv
+from pathlib import Path
+from oscpy.server import OSCThreadServer
+from oscpy.client import OSCClient
+import csv,os,sys
+from datetime import datetime
+from time import sleep
+from plyer import notification
+from pathlib import Path
+from plyer import accelerometer,barometer,compass,gps,gravity,gyroscope,humidity,light,proximity,spatialorientation,temperature
+import time
+from oscpy.server import OSCThreadServer
+from oscpy.client import OSCClient
+
+CLIENT = OSCClient('localhost', 3002)
+DEBUG = False
+# enable for debug prints
+DEBUG = False
+sensorList = ["date","time","timestamp","floor","location","accelerometer" , "barometer" , "battery" , "brightness" , "compass" , "gps" ,
+     "gravity" , "gyroscope" , "humidity" , "light" , "proximity" , "spatialorientation","temperature" ]
+# Create a default dict with default values
+d = dict.fromkeys(sensorList,"")
+def sendNotif():
+    pass
+    notification.notify(title="Time Table",message=message) #type:ignore
+def gps_callback(**kwargs):
+    st =  '{lat}:{lon}:{speed}:{bearing}:{altitude}'.format(**kwargs)
+    s = st.split(':')
+    d['gps'] = {"lat":s[0],"lon":s[1],"speed":s[2],"bearing":s[3],"altitude":s[4]}
+            
+def updateValues():
+    print("updating values")
+    try:
+        d['accelerometer'] = accelerometer.acceleration #type:ignore
+    except:
+        print("accelerometer error")
+    try:
+        d['barometer'] = barometer.pressure #type:ignore
+    except:
+        print("barometer error")
+    try:
+        d['battery'] = battery.status #type:ignore
+    except:
+        print("battery error")
+    try:
+        d['brightness'] =  brightness.current_level()  #type:ignore WRITE_SETTINGS permisison needed 
+    except:
+        print("brightness error")
+    try:
+        d['compass'] =  compass.field_uncalib  #type:ignore you get the uncalibrated field along with iron bias estimation    
+    except:
+        print("compass error")
+    try:
+        d['gravity'] =  gravity.gravity #type:ignore
+    except:
+        print("gravity error")
+    try:
+        d['gyroscope'] = gyroscope.rotation #type:ignore
+    except:
+        print("gyroscope error")
+    try:
+        d['humidity'] = humidity.tell #type:ignore
+    except:
+        print("humidity error")
+    try:
+        d['light'] = light.illumination #type:ignore
+    except:
+        print("light error")
+    try:
+        d['proximity'] = proximity.proximity #type:ignore
+    except:
+        print("proximity error")
+    try:
+        d['spatialorientation'] = spatialorientation.orientation #type:ignore
+    except:
+        print("spatialorientation error")
+    try:
+        d['temperature'] = temperature.temperature #type:ignore
+    except:
+        print("temperature error")
+    t = time.time()
+    dt = datetime.fromtimestamp(t)
+    d["date"] = "date:" + dt.strftime("%d/%m/%Y")
+    d["time"] = "time:" + dt.strftime("%H:%M:%S")
+    d['timestamp'] = "timestamp : {}".format(time.time())
+    try:
+        d['battery'] = battery.status #type:ignore BATTERY_STATS permission needed
+    except:
+        print("battery error")
+def writeData():
+    print("Writing data")
+    if Path(addr).exists():
+          with open(addr,'a') as f: #type:ignore
+              csw = csv.DictWriter(f,fieldnames=sensorList)
+              csw.writerow(d)
+if __name__ == '__main__':
+    compass.enable() #type:ignore
+    accelerometer.enable() #type:ignore
+    barometer.enable() #type:ignore
+    gyroscope.enable() #type:ignore
+    gravity.enable() #type:ignore
+    humidity.enable() #type:ignore
+    light.enable() #type:ignore
+    proximity.enable() #type:ignore
+    temperature.enable() #type:ignore
+    spatialorientation.enable_listener() #type:ignore
+    #   On Android `INTERNET`, `ACCESS_FINE_LOCATION`, `ACCESS_COARSE_LOCATION` permissions are needed for gps
+    gps.configure(on_location=gps_callback) #type:ignore
+    gps.start() #type:ignore
+    from android.storage import primary_external_storage_path                   #type: ignore     
+    dir = primary_external_storage_path()
+    download_dir_path = os.path.join(dir, 'Download')                           #type: ignore
+    addr = download_dir_path + "/values.csv"
+    if not Path(addr).exists():
+      with open(addr,'w') as f: #type:ignore
+            csw = csv.DictWriter(f,fieldnames=sensorList)
+            csw.writeheader()  
+    SERVER = OSCThreadServer()
+    SERVER.listen('localhost', port=3000, default=True)
+    # The getinfo will recieve the floor number and the location of the room/thing.
+    def getInfo(message):
+        mess = message.decode("utf-8")
+        splitt = mess.split(";")
+        if DEBUG : print('was pinged with floor data !')
+        print(splitt)
+        d["floor"] = splitt[0]
+        d["location"] = splitt[1]
+    SERVER.bind(b'/ping',getInfo)
+    while True:
+        updateValues()
+        writeData()
+        sleep(5)
